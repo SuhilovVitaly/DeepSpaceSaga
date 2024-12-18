d662ffbed8dc4eaca02882ced027736e567aea4a
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2024-12-18 23:28:44 +0200

    DSS-62 Commands Refactoring

--- DeepSpaceSaga.UI/GameManager.cs
+++ DeepSpaceSaga.UI/GameManager.cs
@@ -3,17 +3,29 @@
 public class GameManager : IDisposable
 {
     public OuterSpace OuterSpace { get; set; } = new OuterSpace();
-    public EventManager EventController { get; set; }
+    private EventManager _eventManager { get; set; }
+    private bool disposed;
+
+    public EventManager EventController
+    {
+        get => _eventManager;
+        private set => _eventManager = value ?? throw new ArgumentNullException(nameof(value));
+    }
 
     public GameManager(EventManager eventManager)
     {
-        EventController = eventManager;
+        _eventManager = eventManager;
 
+        SubscribeToEvents();
+    }
+
+    private void SubscribeToEvents()
+    {
         EventController.OnSelectCelestialObject += OuterSpace.EventController_OnSelectCelestialObject;
         EventController.OnUnselectCelestialObject += OuterSpace.EventController_OnUnselectCelestialObject;
         EventController.OnShowCelestialObject += OuterSpace.EventController_OnShowCelestialObject;
         EventController.OnHideCelestialObject += OuterSpace.EventController_OnHideCelestialObject;
-    }    
+    }
 
     public ISpacecraft GetPlayerSpacecraft()
     {
@@ -35,8 +47,29 @@ public class GameManager : IDisposable
         await EventController.ExecuteCommandAsync(command);
     }
 
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                EventController.OnSelectCelestialObject -= OuterSpace.EventController_OnSelectCelestialObject;
+                EventController.OnUnselectCelestialObject -= OuterSpace.EventController_OnUnselectCelestialObject;
+                EventController.OnShowCelestialObject -= OuterSpace.EventController_OnShowCelestialObject;
+                EventController.OnHideCelestialObject -= OuterSpace.EventController_OnHideCelestialObject;
+            }
+            disposed = true;
+        }
+    }
+
     public void Dispose()
     {
-        // Освобождение ресурсов
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~GameManager()
+    {
+        Dispose(false);
     }
 }
