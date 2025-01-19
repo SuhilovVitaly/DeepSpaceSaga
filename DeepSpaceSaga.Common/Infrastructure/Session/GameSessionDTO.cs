7018810fc36d78410fc0391be3f0ce20c12a6ab1
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2025-01-19 16:18:08 +0200

    DSSG-65 Migration Session to DTO - I - Infrastructure

---
+++ DeepSpaceSaga.Common/Infrastructure/Session/GameSessionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DeepSpaceSaga.Common.Infrastructure.Session;
+
+public class GameSessionDTO
+{
+    public CelestialMap? CelestialMap { get; set;}
+
+    public Journal? Logbook { get; set; }
+
+    public GameState? State { get; set; } 
+
+    public GameActionEvents? Events { get; set; }
+}
---
+++ DeepSpaceSaga.Common/Infrastructure/Session/SessionMapper.cs
@@ -0,0 +1,16 @@
+﻿using LanguageExt;
+
+namespace DeepSpaceSaga.Common.Infrastructure.Session;
+
+public static class SessionMapper
+{
+    public static GameSessionDTO ToSessionDto(this GameSession session)
+    {
+        return new GameSessionDTO { 
+            CelestialMap = session.SpaceMap.Copy(),
+            Logbook = session.Logbook.Copy(),
+            State = session.State.Copy(),
+            Events = session.Events.Copy(),
+        };
+    }
+}
